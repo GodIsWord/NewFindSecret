bccdf4d7fd734f4c15dfc4a90f182843ba39a480
hezongzhu <hezongzhu@gmail.com>
2018-07-28 11:24:00 +0800

    更新项目xcodeproj版本号

--- FindSecret/FindSecret/ViewController.m
+++ FindSecret/FindSecret/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "XBVideoEditController.h"
 #import <MobileCoreServices/UTCoreTypes.h>
 @interface ViewController () <UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 @property (nonatomic, strong) NSDictionary *userInfo;

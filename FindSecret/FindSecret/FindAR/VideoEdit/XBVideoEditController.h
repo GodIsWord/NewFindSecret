b752131436484e72dd36d8140b2767374f41a795
hezongzhu <hezongzhu@gmail.com>
2018-07-28 11:18:25 +0800

    添加目录

---
+++ FindSecret/FindSecret/FindAR/VideoEdit/XBVideoEditController.h
@@ -0,0 +1,13 @@
+//
+//  XBVideoEditController.h
+//  FindSecret
+//
+//  Created by 9f on 2018/7/28.
+//  Copyright © 2018年 yidezhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XBVideoEditController : NSObject
+
+@end
---
+++ FindSecret/FindSecret/FindAR/VideoEdit/XBVideoEditController.m
@@ -0,0 +1,13 @@
+//
+//  XBVideoEditController.m
+//  FindSecret
+//
+//  Created by 9f on 2018/7/28.
+//  Copyright © 2018年 yidezhang. All rights reserved.
+//
+
+#import "XBVideoEditController.h"
+
+@implementation XBVideoEditController
+
+@end

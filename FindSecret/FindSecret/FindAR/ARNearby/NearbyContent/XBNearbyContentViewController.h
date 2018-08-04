887e9c142d1dbce7ad095148ddb4ee14b28dfc03
zhangyide <zhangyide@9fbank.cc>
2018-08-04 17:28:55 +0800

    添加附近的viewcontroller

---
+++ FindSecret/FindSecret/FindAR/ARNearby/NearbyContent/XBNearbyContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  XBNearbyContentViewController.h
+//  FindSecret
+//
+//  Created by yidezhang on 2018/8/4.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XBNearbyContentViewController : UIViewController
+
+@end
---
+++ FindSecret/FindSecret/FindAR/ARNearby/NearbyContent/XBNearbyContentViewController.m
@@ -0,0 +1,35 @@
+//
+//  XBNearbyContentViewController.m
+//  FindSecret
+//
+//  Created by yidezhang on 2018/8/4.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import "XBNearbyContentViewController.h"
+
+@interface XBNearbyContentViewController ()
+
+@property(nonatomic,strong) UICollectionView *collectionView;
+
+@end
+
+@implementation XBNearbyContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(void)initConllectionView{
+    
+//    _customLayout = [[CustomCollectionViewLayout alloc] init]; // 自定义的布局对象
+//    _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:_customLayout];
+//    _collectionView.backgroundColor = [UIColor whiteColor];
+//    _collectionView.dataSource = self;
+//    _collectionView.delegate = self;
+//    [self.view addSubview:_collectionView];
+    
+}
+
+@end
